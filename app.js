5cb2d484a6bb244b264aa1d73abc89542e8b0155
omar <omar00nasr00@gmail.com>
2021-09-08 16:48:00 +0200

    main-commit

---
+++ app.js
@@ -0,0 +1,125 @@
+
+//functions of animation
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".containerr");
+
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+  
+});
+// all inputs used
+//inputs of create new email
+var signupName = document.getElementById('signupName') 
+var signupEmail = document.getElementById('signupEmail')
+var signupPassword = document.getElementById('signupPassword')
+//inputs fildes in login
+var signinEmail = document.getElementById('signinEmail')
+var signinPassword = document.getElementById('signinPassword')
+    // to get base url (localhost)
+var pathparts = location.pathname.split('/');
+var baseURL = ''
+for (var i = 0; i < pathparts.length - 1; i++) {
+    baseURL += '/' + pathparts[i]
+}
+
+var signUpArray = []
+if (localStorage.getItem('users') == null) {
+    signUpArray = []
+} else {
+    signUpArray = JSON.parse(localStorage.getItem('users'))
+}
+
+
+
+// for check email is exist
+function isEmailExist() {
+    for (var i = 0; i < signUpArray.length; i++) {
+        if (signUpArray[i].email.toLowerCase() == signupEmail.value.toLowerCase()) {
+            return false
+        }
+    }
+}
+//for check inputs is empty or not
+function isEmpty() {
+
+    if (signupName.value == "" || signupEmail.value == "" || signupPassword.value == "") {
+        return false
+    } else {
+        return true
+    }
+}
+function signUp() {
+    if (isEmpty() == false) {
+        document.getElementById('exist').innerHTML = '<span class="text-danger">- All inputs is required -</span>'
+        return false
+    }
+    // store name , email , password as object
+    var signUp = {
+        name: signupName.value,
+        email: signupEmail.value,
+        password: signupPassword.value,
+    }
+    if (signUpArray.length == 0) {
+        signUpArray.push(signUp)
+        localStorage.setItem('users', JSON.stringify(signUpArray))
+        document.getElementById('exist').innerHTML = '<span class="text-success m-3">Success</span>'
+        return true
+    }
+    if (isEmailExist() == false) {
+        document.getElementById('exist').innerHTML = '<span class="text-danger m-3">email already exists</span>'
+
+    } else {
+        signUpArray.push(signUp)
+        localStorage.setItem('users', JSON.stringify(signUpArray))
+        document.getElementById('exist').innerHTML = '<span class="text-success m-3">Success</span>'
+    }
+}
+
+// login function
+//for check inputs is empty or not
+function isLoginEmpty() {
+
+    if (signinPassword.value == "" || signinEmail.value == "") {
+        return false
+    } else {
+        return true
+    }
+}
+
+function login() {
+    if (isLoginEmpty() == false) {
+        document.getElementById('incorrect').innerHTML = '<span class="text-danger m-3">All inputs is required</span>'
+        return false
+    }
+    var password = signinPassword.value
+    var email = signinEmail.value
+    for (var i = 0; i < signUpArray.length; i++) {
+        if (signUpArray[i].email.toLowerCase() == email.toLowerCase() && signUpArray[i].password.toLowerCase() == password.toLowerCase()) {
+            localStorage.setItem('newSession', signUpArray[i].name)
+            if (baseURL == '/') {
+                location.replace('https://' + location.hostname + '/home.html')
+
+            } else {
+                location.replace(baseURL + '/home.html')
+
+            }
+        } else {
+            document.getElementById('incorrect').innerHTML = '<span class="p-2 text-danger">incorrect email or password</span>'
+        }
+    }
+
+}
+// for logout
+function logout() {
+    localStorage.removeItem('newSession')
+}
+//for clear local storage
+// function deleteItems() {
+//     localStorage.clear();
+// }
+  

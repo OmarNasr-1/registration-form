21228023d866a3a516d573cd7f2ac290da9e9f70
omar <omar00nasr00@gmail.com>
2021-09-13 23:48:53 +0200

    responsive

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 //functions of animation
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
-const container = document.querySelector(".containerr");
+const container = document.querySelector(".container");
 
 sign_up_btn.addEventListener("click", () => {
   container.classList.add("sign-up-mode");
